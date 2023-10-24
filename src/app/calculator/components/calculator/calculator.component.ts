e988682294a8619e6e826511014ba6790e422393
vladimarkova <vladislavam1996@gmail.com>
2023-10-25 00:24:38 +0300

    add to handling user input and adding this input to memory (case with many unwanted zeros and only decimal); display tweaks.

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -15,26 +15,37 @@ export interface IInputType {
 export class CalculatorComponent {
   memory: string[] | null = null;
 
+  largeFont: string = '3rem';
+  smallFont = '1.5rem';
+
   get result() {
     if (!this.memory) {
-      return 0;
+      return '0';
     }
     if (this.memory.find(el => el === 'NaN')) {
       return 'NaN';
     }
-    // TODO: use pipe in html
-    if (this.memory[0].endsWith('.')) {
-      return this.memory[0].replace('.', ',');
-    }
-    if (this.memory[2]?.endsWith('.')) {
-      return this.memory[2].replace('.', ',');
-    }
-    return this.memory[2] ? parseFloat(this.memory[2]).toString().replace('.', ',') : parseFloat(this.memory[0]).toString().replace('.', ',');
+    // TODO: use pipe in htmls
+
+    return this.memory[2] ?
+      this.cutString(this.memory[2], '.').replace('.', ','):
+      this.cutString(this.memory[0], '.').replace('.', ',');
   }
 
   symbolType = SymbolType;
 
   constructor() { }
+
+  cutString(value: string, specificSymbol: string) {
+    const index = value.indexOf(specificSymbol);
+
+    if (index !== -1) {
+      const result = value.substring(index - 15, index + 16);
+      return result;
+    } else {
+      return value;
+    }
+  }
 
   handleButtonClick(input: IInputType) {
     const { type, value } = input;
@@ -47,6 +58,18 @@ export class CalculatorComponent {
         return;
       }
       this.memory = [value];
+      return;
+    }
+    if (this.memory[0] === '0' && !this.memory[0].includes('.') && value === '0') {
+      return;
+    }
+    if (this.memory[2] && this.memory[2] === '0' && !this.memory[2].includes('.') && value === '0') {
+      return;
+    }
+    if (this.memory[0]?.includes('.') && !this.memory[1] && !this.memory[2] && type === SymbolType.DECIMAL_POINT) {
+      return;
+    }
+    if (this.memory[2]?.includes('.') && type === SymbolType.DECIMAL_POINT) {
       return;
     }
     if (this.memory[2] && type === SymbolType.OPERATION) {
@@ -82,21 +105,34 @@ export class CalculatorComponent {
     // , (decimal point) also is added here
     if (!this.memory) {
       this.memory = [digit];
+      if (this.memory[0] === '.') {
+        this.memory[0] = '0.';
+      }
+      return;
     }
     if (side === 'left') {
-      if (!this.memory?.[0]) {
-        this.memory[0] = digit;
+      if (!this.memory?.[0] || this.memory[0] === '0') {
+        this.memory[0] = digit === '.' ? '0.' : digit;
+        return;
       }
       const current = this.memory[0];
       this.memory[0] = current + digit;
     } else {
-      if (!this.memory?.[2]) {
-        this.memory[2] = digit;
+      if (this.memory[2] === '.') {
+        this.memory[2] = '0.';
+        return;
+      }
+      if (!this.memory?.[2] || this.memory[2] === '0') {
+        this.memory[2] = digit === '.' ? '0.' : digit;
         return;
       }
       const current = this.memory![2];
       this.memory![2] = current + digit;
     }
+  }
+
+  undoLast() {
+    console.log('Implement me...!');
   }
 
   calculate() {
