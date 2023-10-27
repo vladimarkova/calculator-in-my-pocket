b310dcc6ffd8232db3bc0a2a2788a4012e174881
vladimarkova <vladislavam1996@gmail.com>
2023-10-27 23:55:17 +0300

    add to select theme handling [WIP].

--- src/app/themes/components/theme-select-list/theme-select-list.component.ts
+++ src/app/themes/components/theme-select-list/theme-select-list.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { loadThemes } from 'src/app/+store/actions/theme';
+import { filter, Subscription, take } from 'rxjs';
+import { loadThemes, selectTheme } from 'src/app/+store/actions/theme';
 import { selectSelectedTheme, selectThemes, selectThemesError, selectThemesLoading, selectTotalCount } from 'src/app/+store/selectors/theme';
 
 @Component({
@@ -9,17 +10,44 @@ import { selectSelectedTheme, selectThemes, selectThemesError, selectThemesLoadi
   templateUrl: './theme-select-list.component.html',
   styleUrls: ['./theme-select-list.component.scss']
 })
-export class ThemeSelectListComponent implements OnInit {
+export class ThemeSelectListComponent implements OnInit, OnDestroy {
   themes$ = this.themeStore.select(selectThemes);
   totalCount$ = this.themeStore.select(selectTotalCount);
   loading$ = this.themeStore.select(selectThemesLoading);
   error$ = this.themeStore.select(selectThemesError);
 
   selectedTheme$ = this.themeStore.select(selectSelectedTheme);
+  sub!: Subscription;
 
-  constructor(private themeStore: Store, private route: ActivatedRoute) { }
+  constructor(private themeStore: Store, private router: Router, private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.themeStore.dispatch(loadThemes());
+    this.sub = this.selectedTheme$.subscribe(selectedTheme => {
+      const queryParams = this.router.parseUrl(this.router.url).queryParams;
+      const id = queryParams['theme']
+      if (!selectedTheme && id) {
+        this.router.navigate([], {relativeTo: this.activatedRoute});
+      }
+    })
+  }
+
+  applyTheme(id: string){
+    this.themeStore.dispatch(selectTheme({ id }));
+    this.addThemeIdToParams(id);
+  }
+
+  addThemeIdToParams(id: string) {
+    const currentUrl = this.router.url;
+    const queryParams = this.router.parseUrl(currentUrl).queryParams;
+    queryParams['theme'] = id;
+    this.router.navigate([], {
+      relativeTo: this.activatedRoute,
+      queryParams: queryParams,
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 }
