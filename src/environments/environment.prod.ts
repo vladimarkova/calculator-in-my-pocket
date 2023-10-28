8d20960491038c9e64aae21492f7545889cb2d9e
vladimarkova <vladislavam1996@gmail.com>
2023-10-28 11:01:38 +0300

    add to environment.prod.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   development: false,
-  appUrl: 'vladimarkova.github.io/calculator-in-my-pocket/home', // https://.
-  // url: '', // TODO ...
+  appUrl: "vladimarkova.github.io/calculator-in-my-pocket/home", // https://.
+  url: "https://calculator-in-my-pocket-backend.onrender.com."
 }
